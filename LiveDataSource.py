49649ce499438e2080b63cfcc7d2a225586a14cf
Jon Little <burundiocibu@gmail.com>
2023-09-25 19:04:42 -0500

    for botanist

--- LiveDataSource.py
+++ LiveDataSource.py
@@ -60,7 +60,7 @@ class LiveDataSource:
             logger.debug(f"thread {self.io_thread.name} started")
         elif source.startswith("udp:"):
             port = int(source.removeprefix("udp:"))
-            addr = "192.168.1.197"
+            addr = "192.168.1.138"
             self.io_thread = threading.Thread(target=self.udp_rx, args=(addr, port))
             self.io_thread.setDaemon(daemon_me)
             self.io_thread.start()
